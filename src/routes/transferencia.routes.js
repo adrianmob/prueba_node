cf3e11040f0e602343ed790e9959f30704cddd80
Adrian Quiroz <adrian.quirozpal@gmail.com>
2021-03-12 03:04:35 -0600

    configutacion final

--- src/routes/transferencia.routes.js
+++ src/routes/transferencia.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 import * as tranCtrl from "../controllers/transferencia.controller";
-import { verifyToken } from "../middleware/authjwt";
+import { verifyToken } from "../middleware/authJwt";
 
 router.post("/", verifyToken, tranCtrl.createTransferencias);
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 import * as userCtrl from "../controllers/user.controller";
-import { verifyToken } from "../middleware/authjwt";
+import { verifyToken } from "../middleware/authJwt";
 
 router.post("/", verifyToken, userCtrl.createHolder);
 router.post("/deposito", verifyToken, userCtrl.createDeposito);
